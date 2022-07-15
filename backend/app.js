8d3747b9e1aac8c7b293147e45c411a77b50bbba
MHolt193 <76035004+MHolt193@users.noreply.github.com>
2022-07-15 11:21:01 -0600

    continued work on backend  to allow CORS and listen for image upload

--- backend/app.js
+++ backend/app.js
@@ -10,6 +10,18 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+//cors
+app.use( (req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "*")
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header("Access-Controll-Allow-Credentials", true)
+    next();
+});
+
+
+app.use('/api/images', require('./routes/imageRoutes'))
+
 app.listen( port, ()=>{
     console.log(`Server Listening on ${port}`);
 })
