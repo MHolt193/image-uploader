2e9e41b6b10e5d892dbf3c1113e8a408cc8dffec
MHolt193 <76035004+MHolt193@users.noreply.github.com>
2022-07-16 11:18:54 -0600

    backend work
    allowed frontend to send files to uploads folder and have server send link to said file
    created api route and controller to GET image from server

--- backend/routes/imageRoutes.js
+++ backend/routes/imageRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { getImage, uploadImage } = require('../controllers/imageController')
 
-router.get('/:id', getImage);
+router.get('/:fileName', getImage);
 router.post("/", uploadImage);
 
 module.exports = router;
