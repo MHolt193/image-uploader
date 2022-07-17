471accdc6f3d3d624ddf5048ff4785a8db143c28
MHolt193 <76035004+MHolt193@users.noreply.github.com>
2022-07-16 23:01:42 -0600

    split backend and frontend

--- backend/routes/imageRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getImage, uploadImage } = require('../controllers/imageController')
-
-router.get('/:fileName', getImage);
-router.post("/", uploadImage);
-
-module.exports = router;
